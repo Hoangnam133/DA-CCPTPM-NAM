d74abe5812a13b7c33e97a432ef96978275003aa
dat <trongdat232003@gmail.com>
2025-01-03 00:56:56 +0700

    #1 feature/Dat-core

--- src/core/successResponse.js
+++ src/core/successResponse.js
@@ -3,16 +3,6 @@ const StatusCode = {
   CREATE: 201,
 };
 
-class CREATE extends SuccessResponse {
-  constructor(
-    message,
-    statusCode = StatusCode.CREATE,
-    reasonStatusCode = ReasonStatusCode.CREATE,
-    metaData
-  ) {
-    super({ message, statusCode, reasonStatusCode, metaData });
-  }
-}
 class OK extends SuccessResponse {
   constructor(message, metaData) {
     super({ message, metaData });
